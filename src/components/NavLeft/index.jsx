a19bbe77b3fe7200be6ee7e8418b87d23771405a
yesokimfine <1477677742@qq.com>
2022-01-25 17:49:18 +0800

    3-6菜单渲染

--- src/components/NavLeft/index.jsx
+++ src/components/NavLeft/index.jsx
@@ -4,16 +4,31 @@ import './index.less';
 import MenuList from '../../config/menuConfig'
 const SubMenu = Menu.SubMenu;
 export default class NavLeft extends Component {
+  componentWillMount(){
+    const menuTree = this.mapMenuList(MenuList);
+    this.setState({menuTree})
+  }
+  mapMenuList = (array)=>{
+    return array.map(item=>{
+      if(item.children){
+        return(
+          <SubMenu title={item.title} key={item.key}>
+            {this.mapMenuList(item.children)}
+          </SubMenu>
+        )
+      }
+      return <Menu.Item key={item.key}>{item.title}</Menu.Item>
+    })
+  }
+
   render() {
+    let {menuTree} = this.state
     return <div>
       <div className="logo">
-        <img src="/assets/logo-ant.svg" alt="" />
-        <h1>MyMs</h1>
+        <img src="/assets/bic-logo.jpg" alt="" />
+        <h1>单车系统</h1>
         <Menu theme="dark">
-          <Menu.Item>菜单项</Menu.Item>
-          <SubMenu title="子菜单">
-            <Menu.Item>子菜单项</Menu.Item>
-          </SubMenu>
+          {menuTree}
         </Menu>
       </div>
     </div>;
