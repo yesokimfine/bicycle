4c2b47369dd60c637d396b2df1801359567a7ef1
yesokimfine <1477677742@qq.com>
2022-01-25 12:15:30 +0800

    3-5

--- src/components/NavLeft/index.jsx
+++ src/components/NavLeft/index.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-
+import { Menu, Icon } from 'antd';
+import './index.less';
+import MenuList from '../../config/menuConfig'
+const SubMenu = Menu.SubMenu;
 export default class NavLeft extends Component {
   render() {
     return <div>
-        NavLeft
+      <div className="logo">
+        <img src="/assets/logo-ant.svg" alt="" />
+        <h1>MyMs</h1>
+        <Menu theme="dark">
+          <Menu.Item>菜单项</Menu.Item>
+          <SubMenu title="子菜单">
+            <Menu.Item>子菜单项</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </div>
     </div>;
   }
 }
