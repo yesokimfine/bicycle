e8c6a1e97cf4ed5dea1166aa7319b46a19d8a4f4
yesokimfine <1477677742@qq.com>
2022-01-27 19:16:37 +0800

    3-9底部组件和Home页实现

--- src/admin.jsx
+++ src/admin.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import {Row,Col} from 'antd'
+import { Row, Col } from 'antd'
 import './style/common.less'
 import NavLeft from './components/NavLeft'
 import Header from './components/Header';
+import Home from './page/home';
 import Footer from './components/Footer';
-
 export default class Admin extends Component {
   render() {
     return (
-        <Row className="container">
-            <Col span={4} className="nav-left">
-                <NavLeft/>
+      <Row className="container">
+        <Col span={4} className="nav-left">
+          <NavLeft />
+        </Col>
+        <Col span={20} className="main">
+          <Header />
+          <Row className="content">
+            <Col span={24}>
+              <Home />
             </Col>
-            <Col span={20} className="main">
-                <Header/>
-                <Row className="content">
-                  Content
-                </Row>
-                <Footer/>
-            </Col>
-        </Row>
+          </Row>
+          <Footer />
+        </Col>
+      </Row>
     );
   }
 }
