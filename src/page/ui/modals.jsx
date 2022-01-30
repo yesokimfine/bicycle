02838139ab2457eea79588b1bfef9d0a6cd4b207
yesokimfine <1477677742@qq.com>
2022-01-30 15:39:31 +0800

    5-4 UI/弹框界面实现

--- src/page/ui/modals.jsx
+++ src/page/ui/modals.jsx
@@ -18,6 +18,39 @@ export default class Modals extends Component {
     setTimeout(() => {
       this.setState({ isLoading: false })
     }, 2000)
+  };
+  handdleConfirm = (type) => {
+    let content = "";
+    let okText = "确定";
+    let cancelText = "取消";
+    switch (type) {
+      case "confirm":
+        content = "确定要离开当前页面吗";
+        okText = "再逛逛";
+        cancelText = "坚持退出"
+        break;
+      case "info":
+        content = "您无权访问！";
+        break;
+      case "success":
+        content = "购买成功！凭证码稍后将以短信形式发送到绑定手机，请您留意查收。"
+        break;
+      case "error":
+        content="页面加载超时！请刷新页面重新登录";
+        break;
+    }
+    Modal[type]({
+      title: "系统提示",
+      content,
+      okText,
+      cancelText,
+      onOk() {
+        console.log("Ok");
+      },
+      onCancel() {
+        console.log("Cancel");
+      }
+    })
   }
   render() {
     let { mod1, mod2, mod3, mod4, isLoading } = this.state;
@@ -27,6 +60,12 @@ export default class Modals extends Component {
         <Button type="primary" onClick={() => this.handdleOpen("mod2")}>自定义页脚</Button>
         <Button type="primary" onClick={() => this.handdleOpen("mod3")}>距离顶部20px</Button>
         <Button type="primary" onClick={() => this.handdleOpen("mod4")}>水平垂直居中</Button>
+      </Card>
+      <Card title="信息确认框" className="card-wrapper">
+        <Button type="primary" onClick={() => this.handdleConfirm("confirm")}>确认框</Button>
+        <Button type="primary" onClick={() => this.handdleConfirm("info")}>信息框</Button>
+        <Button type="primary" onClick={() => this.handdleConfirm("success")}>成功</Button>
+        <Button type="primary" onClick={() => this.handdleConfirm("error")}>出错</Button>
       </Card>
       <Modal
         title="标题"
@@ -48,7 +87,7 @@ export default class Modals extends Component {
         onOk={() => this.setState({ mod2: false })}
         footer={[
           <Button type="primary" icon={<UploadOutlined />} loading={isLoading} onClick={this.handdleSubmit}>上传</Button>,
-          <Button type="primary" loading={isLoading}>确定</Button>,
+          <Button type="primary" loading={isLoading} onClick={() => this.setState({ mod2: false })}>确定</Button>,
           <Button onClick={() => this.setState({ mod2: false })}>取消</Button>,
         ]}
       >
