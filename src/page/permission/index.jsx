c2e9555082ab723c76b543ce0fd1d83a58b74f2b
yesokimfine <1477677742@qq.com>
2022-03-12 09:40:37 +0800

    14-4 设置权限功能

--- src/page/permission/index.jsx
+++ src/page/permission/index.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import { Button, Card, Table, Modal, Form, Select, message } from "antd";
 import {
-  UserAddOutlined,
-  SettingOutlined,
-  BulbOutlined,
-} from "@ant-design/icons";
+  Button,
+  Card,
+  Table,
+  Modal,
+  Form,
+  Select,
+  message,
+  Input,
+  Tree,
+} from "antd";
+import { UserAddOutlined, SettingFilled, EyeFilled } from "@ant-design/icons";
 import Utils from "../../utils/utils";
+import treeConfig from "../../config/menuConfig";
 
+const treeData = [{ title: "平台权限", key: "all", children: [...treeConfig] }];
 const FormItem = Form.Item;
 const { Option } = Select;
 const columns = [
@@ -39,16 +47,21 @@ const createRoleForm = React.createRef();
 export default class Permission extends Component {
   state = {
     modalType: "",
+    selectedRowKeys: [],
+    selectedItem: {},
+    checkedKeys: [],
   };
   componentWillMount() {
     Utils.myAxios(
       "https://mock.apipost.cn/app/mock/project/2784e323-1389-4f85-a288-74cfbbbf595f/roleList"
     ).then((res) => {
+      res.result.item_list.map((item, index) => (item.key = index));
       this.setState({ dataSource: res.result.item_list });
     });
   }
   render() {
-    let { dataSource, modalType } = this.state;
+    let { dataSource, modalType, selectedRowKeys, selectedItem} =
+      this.state;
     return (
       <div>
         <Card>
@@ -61,17 +74,37 @@ export default class Permission extends Component {
           </Button>
           <Button
             style={{ margin: "0 30px" }}
-            onClick={() => this.setState({ modalType: "set_role" })}
+            onClick={() => {
+              if (Object.keys(selectedItem).length === 0) {
+                Modal.warning({ content: "没有记录被选中" });
+                return;
+              }
+              this.setState({ modalType: "set_role" });
+            }}
           >
-            <SettingOutlined />
+            <SettingFilled />
             设置权限
           </Button>
           <Button onClick={() => this.setState({ modalType: "perm_role" })}>
-            <BulbOutlined />
+            <EyeFilled />
             用户授权
           </Button>
         </Card>
-        <Table columns={columns} dataSource={dataSource} />
+        <Table
+          columns={columns}
+          dataSource={dataSource}
+          bordered
+          rowSelection={{ type: "radio", selectedRowKeys }}
+          onRow={(record, index) => {
+            return {
+              onClick: () => {
+                let selectedRowKeys = [index];
+                let selectedItem = record;
+                this.setState({ selectedRowKeys, selectedItem });
+              },
+            };
+          }}
+        />
         <Modal
           visible={modalType === "create_role"}
           title="创建角色"
@@ -104,6 +137,48 @@ export default class Permission extends Component {
                 <Option value="disabled">停用</Option>
               </Select>
             </FormItem>
+          </Form>
+        </Modal>
+        <Modal
+          title="设置权限"
+          visible={modalType === "set_role"}
+          okText="设置"
+          cancelText="取消"
+          onCancel={() => this.setState({ modalType: "" })}
+          onOk={() => {
+            this.setState({ modalType: "" });
+            this.setState({setRoleForm:this.refs.setRoleForm.getFieldValue()})
+            message.success({
+              content: "设置成功",
+            });
+          }}
+        >
+          <Form ref="setRoleForm">
+            <FormItem
+              label="角色名称"
+              name="role_name"
+              initialValue={selectedItem.role_name}
+            >
+              <Input disabled />
+            </FormItem>
+            <FormItem
+              name="use_status"
+              label="角色状态"
+              initialValue={selectedItem.use_status}
+            >
+              <Select>
+                <Option value="running">启用</Option>
+                <Option value="disabled">停用</Option>
+              </Select>
+            </FormItem>
+              <Tree
+                checkable
+                treeData={treeData}
+                defaultExpandedKeys={["all"]}
+                onCheck={(checkedKeys) => {
+                  this.setState({ checkedKeys });
+                }}
+              />
           </Form>
         </Modal>
       </div>
